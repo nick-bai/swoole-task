a43f97ded128a88899f374b347de1209b2abd26e
白云飞 <876337011@qq.com>
2018-11-06 16:01:19 +0800

    更新 common.php

--- common.php
+++ common.php
@@ -6,29 +6,6 @@
  * Date: 2018/11/6
  * Time: 2:37 PM
  */
-/**
- * 生成订单号
- * @return string
- */
-function makeOrderId() {
-    return date('YmdHis') . str_pad(mt_rand(1, 999999), 5, '0', STR_PAD_LEFT);
-}
-
-function getMsectime() {
-    list($msec, $sec) = explode(' ', microtime());
-    $msectime =  (float)sprintf('%.0f', (floatval($msec) + floatval($sec)) * 1000);
-
-    return $msectime;
-}
-
-/**
- * 格式化毫秒时间
- * @return false|string
- */
-function getMicrotimeFormat() {
-    $time = getMsectime();
-    return date('YmdHis', $time / 1000);
-}
 
 function config($key) {
 
